984f4004c56878bbbadc6f7fd36fc771e7e6dbb8
varun-sharma-estaie <varun@estaie.com>
2025-12-07 23:46:52 +0530

    edit: added chart js data labels CDN

--- resources/themes/anchor/partials/head.blade.php
+++ resources/themes/anchor/partials/head.blade.php
@@ -43,6 +43,7 @@
     rel="stylesheet"
 />
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
 
 @if(isset($seo->description))
     <meta name="description" content="{{ $seo->description }}">
