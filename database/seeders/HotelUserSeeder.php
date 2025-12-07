5ac33407f7f6348e34cb37a27b85f163832d43c6
varun-sharma-estaie <varun@estaie.com>
2025-12-08 01:38:10 +0530

    edit: new pivot table seeder

---
+++ database/seeders/HotelUserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class HotelUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('hotel_user')->delete();
+
+        DB::table('hotel_user')->insert([
+            0 => [
+                "id" => 1,
+                "user_id" => 2,
+                "hotel_id" => 2,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            // 1 => [
+            //     "id" => 2,
+            //     "user_id" => '3',
+            //     "hotel_id" => '2',
+            //     "created_at" => now(),
+            //     "updated_at" => now()
+            // ]
+        ]);
+    }
+}
