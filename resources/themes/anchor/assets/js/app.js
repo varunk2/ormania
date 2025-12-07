7b4bc6eab0577ae0982b38e792bc49c50d1ac57b
varun-sharma-estaie <varun@estaie.com>
2025-12-07 23:47:06 +0530

    edit: globally registered Chart variables

--- resources/themes/anchor/assets/js/app.js
+++ resources/themes/anchor/assets/js/app.js
@@ -1,3 +1,11 @@
+// Register DataLabels plugin
+Chart.register(ChartDataLabels);
+
+// Global Chart Defaults
+Chart.defaults.color = '#8B949E';
+Chart.defaults.borderColor = '#30363D';
+Chart.defaults.font.family = "'Inter', sans-serif";
+
 window.demoButtonClickMessage = function(event){
     event.preventDefault(); new FilamentNotification().title('Modify this button in your theme folder').icon('heroicon-o-pencil-square').iconColor('info').send()
 }
